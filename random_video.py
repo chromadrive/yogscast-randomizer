93c9355053c43ecd01d0b91df933267ca5b56de1
DAVID XIONG <DAVID.W.XIONG@GMAIL.COM>
2017-11-13 19:40:53 -0800

    slight changes to take advantage of new API feature

--- random_video.py
+++ random_video.py
@@ -1,12 +1,12 @@
-import argparse    
+import argparse	
 import json
-from random import randint, choice
+# from random import randint, choice
 try:
-    # For Python 3.0 and later
-    from urllib.request import Request, urlopen
+	# For Python 3.0 and later
+	from urllib.request import Request, urlopen
 except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import Request, urlopen
+	# Fall back to Python 2's urllib2
+	from urllib2 import Request, urlopen
 
 
 def parse_json(url):
@@ -17,21 +17,19 @@ def parse_json(url):
 
 
 def get_random_video_id(channel):
-	url = "https://api.yogsdb.com/api/videos?channel="+ channel +"&orderBy=upload_date|desc"
+	url = "https://api.yogsdb.com/api/videos?channel="+ channel +"&inRandomOrder=1"
 	result = parse_json(url)
 	if not result['data']:
 		return "Could not find channel with ID " + channel
-	random_page = randint(1, int(result['last_page']))
-	url = "https://api.yogsdb.com/api/videos?channel="+ channel +"&orderBy=upload_date|desc&page=" + str(random_page)
-	result = parse_json(url)
-	selected_video = choice(result['data'])
+
+	selected_video = result['data'][0]
 
 	return selected_video['title'] + ": https://www.youtube.com/watch?v=" + selected_video['youtube_id']
 
 
 parser = argparse.ArgumentParser(description='Selects a random video from a Yogscast channel')
 parser.add_argument('channel_id', metavar='channel-id', type=str,
-                   help='A Yogscast channel ID')
+				   help='A Yogscast channel ID')
 args = parser.parse_args()
 
 
